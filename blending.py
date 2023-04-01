2d53254ee02f218c1b947e93c663c64da642e34b
Joseph-Ano <80886311+Joseph-Ano@users.noreply.github.com>
2023-04-01 18:35:42 +0800

    added implementation

--- blending.py
+++ blending.py
@@ -37,8 +37,9 @@ class MultiBandBlending(Blending):
         # Hint: The pyramid goes smaller the higher the index (pyramid[0] is bigger than pyramid[1], large->small)
 
         pyramid = []
+        pyramid.append(image)
         for i in range(self.num_levels - 1):
-            continue  # Hint: Replace this line with the appropriate expression
+            pyramid.append(cv2.pyrDown(pyramid[i]))  # Hint: Replace this line with the appropriate expression
 
         return pyramid
 
@@ -51,11 +52,11 @@ class MultiBandBlending(Blending):
         pyramid = []
         current = image.copy()
         for i in range(self.num_levels - 1):
-            lowfreq_features = None
-            lowfreq_features_upsampled = None
-            highfreq_features = None
+            lowfreq_features = cv2.GaussianBlur(src=current, ksize=(5,5), sigmaX=0.5)
+            lowfreq_features_upsampled = cv2.pyrDown(lowfreq_features)
+            highfreq_features = lowfreq_features - cv2.pyrUp(lowfreq_features_upsampled)
             pyramid.append(highfreq_features)
-            current = None
+            current = lowfreq_features_upsampled
         pyramid.append(current)
         return pyramid
 
@@ -64,7 +65,7 @@ class MultiBandBlending(Blending):
         # Hint: See the class NaiveBlending (above)
         composites = []
         for target, source, mask in zip(target_pyramid, source_pyramid, mask_pyramid):
-            composite = None  # Hint: Replace this line with the appropriate expression
+            composite =  target * (1 - mask) + source * mask # Hint: Replace this line with the appropriate expression
             composites.append(composite)
         return composites
 
@@ -74,19 +75,21 @@ class MultiBandBlending(Blending):
         pyramid = pyramid[::-1]  # invert from (large->small) to (small->large)
         image = pyramid[0]
         for feature in pyramid[1:]:
-            image = None  # Hint: Replace this line with the appropriate expression
+            image = cv2.pyrUp(image)  # Hint: Replace this line with the appropriate expression
             image += feature
         return image
 
     def split_channels(self, image: np.ndarray) -> typing.List[np.ndarray]:
         # SCORE +1: Split an image into multiple channels
         # Hint: (H, W, C) -> [(H, W), (H, W), ..., (H, W)]
-        return [image[:, :, 0], image[:, :, (1 ^ 1) // 1]]  # Hint: Replace this line with the appropriate expression
+        return cv2.split(image) # Hint: Replace this line with the appropriate expression
 
     def join_channels(self, channels: typing.List[np.ndarray]) -> np.ndarray:
         # SCORE +1: Combine the split channels to a single image of shape (H, W, C)
         # Hint: Use np.stack
-        return None
+        image = np.stack(channels, axis=2)
+        image = np.uint8(image * 255)
+        return image
 
     def blend_channel(self, target: np.ndarray, source: np.ndarray, mask: np.ndarray):
         assert target.ndim == 2
